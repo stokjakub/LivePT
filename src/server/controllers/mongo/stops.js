8f64866a8a13b1715b2fc5b3019b5d8a09acdc09
stokjakub <stokjakub@gmail.com>
2016-04-28 17:00:38 +0200

    loading enhancment

--- src/server/controllers/mongo/stops.js
+++ src/server/controllers/mongo/stops.js
@@ -24,11 +24,11 @@ router.get('/getstop', function(req, res){
 router.get('/getStopsInTheArea', function(req, res){
     var coordinates = JSON.parse(req.param('coordinates'));
     var zoom = req.param('zoom');
-    console.log(coordinates.lat);
-
+    console.log(zoom);
+    var factor = zoom * 0.001;
     Stops.find({
-        WGS84_LON: { $gt:coordinates.lng - 0.01 , $lt: coordinates.lng + 0.01 },
-        WGS84_LAT: { $gt: coordinates.lat - 0.01 , $lt: coordinates.lat + 0.01}
+        WGS84_LON: { $gt:coordinates.lng - factor*2 , $lt: coordinates.lng + factor*2 },
+        WGS84_LAT: { $gt: coordinates.lat - factor , $lt: coordinates.lat + factor}
     },function(err, data){
         if(err) console.error();
         res.json(data);
